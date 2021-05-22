1c866a078491f8f47399510d146dd3b9b5634c2b
Varchasa Aggarwal <43700017+varchasa@users.noreply.github.com>
2021-05-22 23:23:26 +0530

    python

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template,request,jsonify
-from flask_mail import Mail, Message
+import firebase
+from firebase import Firebase
 
-import smtplib
 
 app = Flask(__name__)
-mail = Mail(app)
-
-app.config.update(
-	DEBUG=True,
-	#EMAIL SETTINGS
-	MAIL_SERVER='smtp.gmail.com',
-	MAIL_PORT=465,
-	MAIL_USE_SSL=True,
-	MAIL_USERNAME = 'varchasa.tech@gmail.com',
-	MAIL_PASSWORD = '********'
-	)
-mail = Mail(app)
+config = {
+   "apiKey": "**************",
+  "authDomain": "**************",
+  "databaseURL": "**************",
+  "projectId": "**************",
+  "storageBucket": "**************",
+  "messagingSenderId": "**************",
+  "appId": "**************",
+  "measurementId": "**************"
+}
+firebase = Firebase(config)
+db = firebase.database()
 
 @app.route("/")
 def home():
@@ -33,21 +33,11 @@ def send_message():
             name=request.form['name']
             gmail=request.form['email']
             suggestion=request.form['suggestion'] 
-            to = 'varchasa.tech@gmail.com'
-            print("done4")
-        
-    
-            text = Message(name, sender = gmail, recipients=[to])
-            text.body=suggestion+" "+gmail+"\n"+"portfolio data"
-            
-            
-            print("done3")
-            
-            print("done1")
-            mail.send(text)
+            data={'name':name,'email':gmail,'msg':suggestion}
+            db.child("users").push(data)
             return render_template("home.html")
     except:
         return('PLEASE FILL OUT THE REQUIRED DATA')
         
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="192.168.43.141",debug=True)
